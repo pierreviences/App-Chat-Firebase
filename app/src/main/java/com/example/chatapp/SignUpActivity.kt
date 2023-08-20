742f3d27a991d13b9fe3450e2102803378593bb4
pierreviences <imaniyazid@gmail.com>
2023-08-20 14:59:21 +0700

    feat: Initialize Firebase Authentication and Database

--- app/src/main/java/com/example/chatapp/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/SignUpActivity.kt
@@ -1,11 +1,24 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        auth = FirebaseAuth.getInstance()
     }
+
+
+
 }
