0d1be9a928107d7d1f595f2307e2ffcaaf09ae4b
pierreviences <imaniyazid@gmail.com>
2023-08-20 23:21:19 +0700

    feat: Implement image upload functionality

--- app/src/main/java/com/example/chatapp/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/chatapp/activity/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chatapp.activity
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -12,6 +13,8 @@ import com.example.chatapp.R
 import com.example.chatapp.databinding.ActivityProfileBinding
 import com.example.chatapp.databinding.ActivityUsersBinding
 import com.example.chatapp.model.User
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -19,7 +22,12 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.OnProgressListener
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import java.io.IOException
+import java.util.UUID
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
@@ -29,6 +37,9 @@ class ProfileActivity : AppCompatActivity() {
 
     private var filePath: Uri? = null
     private val PICK_IMAGE_REQUEST:Int = 2020
+
+    private lateinit var storage: FirebaseStorage
+    private lateinit var storageRef: StorageReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
@@ -36,6 +47,9 @@ class ProfileActivity : AppCompatActivity() {
 
         firebaseUser = FirebaseAuth.getInstance().currentUser!!
         databaseReference  = FirebaseDatabase.getInstance().getReference("users").child(firebaseUser.uid)
+
+        storage = FirebaseStorage.getInstance()
+        storageRef = storage.reference
         databaseReference.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 val user = snapshot.getValue(User::class.java)
@@ -81,5 +95,35 @@ class ProfileActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
         }
+    }
+
+    private fun uploadImage(){
+        if(filePath != null){
+            val progressDialog: ProgressDialog = ProgressDialog(null)
+            progressDialog.setTitle("Uploading...")
+            progressDialog.show()
+
+            var ref: StorageReference = storageRef.child("image/" + UUID.randomUUID().toString())
+            ref.putFile(filePath!!)
+                .addOnSuccessListener {
+                    OnSuccessListener<UploadTask.TaskSnapshot>{
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext,"Uploaded", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener{
+                    OnFailureListener{
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext,"Failed" + it.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                .addOnProgressListener {
+                    OnProgressListener<UploadTask.TaskSnapshot>{
+                        var progress: Double = (100.0*it.bytesTransferred/it.totalByteCount)
+                        progressDialog.setMessage("Uploaded" + progress.toInt()+"%")
+                    }
+                }
+            }
     }
 }
