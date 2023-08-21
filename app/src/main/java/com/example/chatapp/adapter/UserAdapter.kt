3b44ba7d9d81bcd0980fe2a0069751d290ac9091
pierreviences <imaniyazid@gmail.com>
2023-08-21 08:29:03 +0700

    Add click listener for user layout in holder

--- app/src/main/java/com/example/chatapp/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/chatapp/adapter/UserAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.chatapp.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.chatapp.R
+import com.example.chatapp.activity.ChatActivity
 import com.example.chatapp.model.User
 import de.hdodenhof.circleimageview.CircleImageView
 
@@ -17,6 +20,7 @@ class UserAdapter(private val context: Context, private val userList:ArrayList<U
         val txtUsername: TextView = view.findViewById(R.id.userName)
         val txtTemp: TextView = view.findViewById(R.id.temp)
         val imgUser: CircleImageView = view.findViewById(R.id.userImage)
+        val layoutUser: LinearLayout = view.findViewById(R.id.layoutUser)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -32,5 +36,11 @@ class UserAdapter(private val context: Context, private val userList:ArrayList<U
         val user = userList[position]
         holder.txtUsername.text = user.userName
         Glide.with(context).load(user.profileImage).placeholder(R.drawable.profile_image).into(holder.imgUser)
+
+        holder.layoutUser.setOnClickListener {
+            val intent  = Intent(context, ChatActivity::class.java)
+            intent.putExtra("UserId",user.userId)
+            context.startActivity(intent)
+        }
     }
 }
