75b850c318c3d84dc8048b0d18d875e79bf44a0d
pierreviences <imaniyazid@gmail.com>
2023-08-21 16:46:14 +0700

    feat: Added Retrofit Network Instance for Chat App

--- app/src/main/java/com/example/chatapp/constans/Constans.kt
+++ app/src/main/java/com/example/chatapp/constans/Constans.kt
@@ -1,6 +1,6 @@
 package com.example.chatapp.constans
 
-class Constans {
+class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
         const val SERVER_KEY = "BLvCZ20BdBFKjBtesPFgZpC4Q2qXWYkRSJNP7mRft9NCAjurUnSwb-siT_CSl0qSnXi6GUZpt5PH73jW5ScE8aQ"
