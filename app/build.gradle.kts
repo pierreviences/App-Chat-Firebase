de262d4f6a25315d98f9f565d8e427bb30e68bec
pierreviences <imaniyazid@gmail.com>
2023-08-21 09:47:02 +0700

    feat: Add Firebase Cloud Messaging constants

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics-ktx:21.3.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
     implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/chatapp/constans/Constans.kt
@@ -0,0 +1,9 @@
+package com.example.chatapp.constans
+
+class Constans {
+    companion object{
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "BLvCZ20BdBFKjBtesPFgZpC4Q2qXWYkRSJNP7mRft9NCAjurUnSwb-siT_CSl0qSnXi6GUZpt5PH73jW5ScE8aQ"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
